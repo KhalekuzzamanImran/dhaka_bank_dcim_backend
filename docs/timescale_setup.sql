aefe228b6570d87bb7442a261af885f475138270
Khalekuzzaman Imran <khalekuzzamanimran@gmail.com>
2026-06-28 18:41:47 +0600

    Add gitignore for Django DCIM project

--- docs/timescale_setup.sql
+++
@@ -1,9 +0,0 @@
-CREATE EXTENSION IF NOT EXISTS timescaledb;
-SELECT create_hypertable('telemetry_points', 'time', if_not_exists => TRUE);
-ALTER TABLE telemetry_points SET (
-  timescaledb.compress,
-  timescaledb.compress_segmentby = 'device_id, metric_id'
-);
-SELECT add_compression_policy('telemetry_points', INTERVAL '7 days');
--- Adjust retention according to bank policy.
--- SELECT add_retention_policy('telemetry_points', INTERVAL '365 days');
